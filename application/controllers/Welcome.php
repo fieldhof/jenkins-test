ebc1ba7e888d21bc561d594c53d051c53d3f75c9
fieldhof <daanveldhof@gmail.com>
2017-11-28 16:54:20 +0100

    Added namespace to Welcome Controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
 namespace Controller;
+
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
